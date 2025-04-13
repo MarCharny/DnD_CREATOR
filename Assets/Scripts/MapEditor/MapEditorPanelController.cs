a46d9d5ac52947a002edcba39f457664a37f2cb8
Marina Charnaya <charnaia.mm@phystech.edu>
2025-04-13 23:19:47 +0300

    MainMenu

--- Assets/Scripts/MapEditor/MapEditorPanelController.cs
+++ Assets/Scripts/MapEditor/MapEditorPanelController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClickInteraction : MonoBehaviour
+public class MapEditorPanelController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
