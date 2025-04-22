37b62fa1e8537978d1d8394697813f7680e1a48f
Marina Charnaya <charnaia.mm@phystech.edu>
2025-04-22 07:51:16 +0300

    Save

--- Assets/Scripts/AIManagment/AIReplicateManager.cs
+++ Assets/Scripts/AIManagment/AIReplicateManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.Networking;
 
 public class AIReplicateManager : AIManagerInterface
 {
-
     public AIReplicateManager()
     {
         m_apiKey = GetEnviromentAPIKey();
---
+++ Assets/Scripts/SaveSystem/DataStructure.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Location
+{
+    public string locationName;
+    public string style;
+    public string commonDesc;
+    public string styleDesc;
+    public string objectsDesc;
+    public string pathToMapImage;
+    public string parentMapName;
+    public Vector2 positionOnParentMap;
+    public List<Location> subLocations;
+}
+
+[System.Serializable]
+public class Character
+{
+    public string characterName;
+    public uint level;
+    public uint age;
+    public int characterClassIdx;
+    public int characterRaceIdx;
+    public string history;
+    public string appearance;
+    public string clothes;
+    public string pathToFullBodyImage;
+    public string pathToAvatarImage;
+}
